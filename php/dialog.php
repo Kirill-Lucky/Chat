0257dddc11a8b59f68fd29ee49b773590511d6be
Kirill <53191352+Kirill-Lucky@users.noreply.github.com>
2022-03-15 21:14:34 +0100

    Update dialog.php

--- php/dialog.php
+++ php/dialog.php
@@ -2,25 +2,25 @@
 <div id="dialog">
 	<div id="message-list">
 		<ul>
-		<!-- Вывод запроса на демонстрацию сообщений пользователя, который авторизован -->
+		<!-- Displaying a request to display messages of a user who is authorized -->
 		<?php
 		if(isset($_GET["user"])){
 			$sql = "SELECT * FROM messages" . 
 					" WHERE (id_user_to = " . $_GET["user"] ." AND id_user_from = " . $_COOKIE["id"] . ") 
 						OR (id_user_to = " . $_COOKIE["id"] ." AND id_user_from=" . $_GET["user"] . ")";
-			//создаем перемнную результат запроса (массив)
+			//create query result variable (array)
 			$result = mysqli_query($connect, $sql);
 			
 			$count_message = mysqli_num_rows($result);
 			$i = 0;
-			// Перебор сообщений, которые соответствуюи запросу для вывода на экран	
+			// Iterating over messages that match a query for display
 			while ($i < $count_message) {
-				//берем результат запроса и превращаем его в массив для использования
+				//take the query result and turn it into an array to use
 				$all_message = mysqli_fetch_assoc($result);
 					echo "<li>";
-						// Запрос для вывода аватарки и имени из таблицы пользователей путем сравнивания id
+						// Query to display avatar and name from user table by comparing id
 						$sql1 = "SELECT * FROM user WHERE id ='" . $all_message["id_user_from"] . "'";
-						//если я правильно помню это подключение базы и запроса для выполнения
+						//if I remember correctly this is the connection of the base and the query to execute
 						$result1 = mysqli_query($connect, $sql1);
 						$users = mysqli_fetch_assoc($result1);
 						echo "<div class=\"avatar\">
@@ -42,7 +42,7 @@
 		?> 
 		</ul>
 		<?php
-		// Добавление сообщений в базу данных, а также айди пользователей, для их последеющего отображения
+		// Adding messages to the database, as well as user IDs, for their subsequent display
 		if(isset($_POST["text"]) && $_POST["text"]!=""){
 			$sql = "INSERT INTO messages (text, id_user_to, id_user_from) VALUES ('". $_POST["text"] ."', '" . $_POST["id_user_to"] ."', '" . $_COOKIE["id"] . "')";
 			if(mysqli_query($connect, $sql)){
@@ -56,8 +56,8 @@
 	</div>
 
 	<?php 
-	// Условие для того чтобы при выводе страницы, когда не выбран пользователь не выдавало ошибку
-	// А также чтобы при отправке сообщения, пользователь оставаля на странице чата
+	// Condition so that when the page is displayed, when the user is not selected, it does not give an error
+	// And also so that when sending a message, the user stays on the chat page
 	if (isset($_GET["user"])) {
 		?>
 		<form id="message-form" action='index.php?user=<?php echo $_GET["user"] ?>' method="POST">
